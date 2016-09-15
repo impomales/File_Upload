0fca6c96418a16571581445b0034b5dff633cb4c
Isaias <impomales@gmail.com>
2016-09-15 03:41:20 +0000

    add index html file

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var app = express()
+var path = require('path')
 var multer = require('multer')
 var upload = multer({dest: 'uploads/'})
 
-
+app.use(express.static(path.join(__dirname, '/static')))
 
 app.get('/', function(req, res) {
     res.send('hello world...')
